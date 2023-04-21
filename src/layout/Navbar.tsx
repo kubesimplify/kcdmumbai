c9cedb3e79a5f7c663cb6aa44687634c128464b3
Saiyam Pathak <saiyam911@gmail.com>
2023-04-21 12:14:37 +0530

    Update Navbar.tsx

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -29,7 +29,7 @@ const Navbar = () => {
           ))}
         </div>
         <div>
-          <Button href="https://konfhub.com/checkout/kcdmumbai?ticketId=7350">
+          <Button href="https://konfhub.com/checkout/kcdmumbai?ticketId=7351">
             Get Your Ticket {TicketLogo}
           </Button>
         </div>
